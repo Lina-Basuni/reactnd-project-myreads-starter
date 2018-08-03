365071b7a959bce9898e00dc8e4df9afe6b25fce
Lina Ahmed Kamal Basuni <lina.ahmedkamal@gmail.com>
2018-08-03 07:15:37 +0200

    code the components html and create state

--- src/mainPage.js
+++ src/mainPage.js
@@ -1,15 +1,32 @@
-import react, {component} from 'react';
+import React, { Component } from 'react';
 
-import book from './book';
+import Book from './Book';
+import Shelf from './Shelf'
 
-class mainPage extends Component{
-  render(
+class MainPage extends Component{
 
+  render(){
     return(
-
+      <div className='main-page'>
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+              <Shelf shelfName="Currently Reading"/>
+              <Shelf shelfName="Want To Read"/>
+              <Shelf shelfName="Read"/>
+            </div>
+          </div>
+          <div className="open-search">
+            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          </div>
+        </div>
+      </div>
     )
+  }
 
-  )
 }
 
-export default mainPage;
+export default MainPage;
